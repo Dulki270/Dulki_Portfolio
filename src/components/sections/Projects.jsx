a60c938f46e5f4b876c0177e634e04d67a69e3ed
Dulki <dulkikavindya@gmail.com>
2025-09-22 09:53:09 +0530

    Added Portfolio project details to Projects section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -40,6 +40,37 @@ export const Projects = () => {
               </div>
             </div>
 
+            {/* ERP System Card */}
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
+              <h3 className="text-xl font-bold mb-2">Portfolio Website</h3>
+              <p className="text-gray-400 mb-4">
+                A personal portfolio website built with React and Tailwind CSS to showcase my projects,
+                skills, and contact information. Deployed using GitHub Pages.
+              </p>
+
+              {/* Tech tags */}
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Tailwind CSS"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] hover:bg-blue-500/20 transition-all"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://github.com/Dulki270/diluclothing.git"
+                  target="_blank"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project
+                </a>
+              </div>
+            </div>
+
             {/* Calculator Card */}
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 transition-all hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]">
               <h3 className="text-xl font-bold mb-2">Web-based Calculator</h3>
